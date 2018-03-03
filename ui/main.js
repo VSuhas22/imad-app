ac6fe76eeaeb015f419e702b47f91d806aa2d88e
Sai Suhas Vemuri <31122182+VSuhas22@users.noreply.github.com>
2018-03-03 21:18:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,10 +2,23 @@ console.log('Loaded!');
 var x=0;
 var element = document.getElementById('img');
 var marginleft=0;
+var y=0;
 function moveright()
 {
+    if(marginleft==1000)
+    y=1;
+    else if(y==1&& marginleft==-1000)
+    y=0;
+    if(y===0)
+    {
     marginleft=marginleft+10;
     img.style.marginLeft=marginleft+'px';
+    }
+    else
+    {
+        marginleft=marginleft+10;
+        img.style.marginLeft=marginleft+'px';
+    }
 }
 img.onclick=function(){
    var interval=setInterval(moveright,10);
