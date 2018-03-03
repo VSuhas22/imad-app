fd0b8ae5422b768b54cc5134529dccf1d6c86e7d
Sai Suhas Vemuri <31122182+VSuhas22@users.noreply.github.com>
2018-03-03 22:46:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,4 @@
-
+window.onload = function(){
 /*console.log('Loaded!');
 var x=0;
 var img = document.getElementById('img');
@@ -47,4 +47,5 @@ butt.onclick=function(){
     
     request.open('GET','http://saisuhasvemuri.imad.hasura-app.io/counter',true);
     request.send(null);
+};
 };
