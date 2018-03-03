b57f7a7b0d9f85ac9f6de06a39cb0beb27d6a078
Sai Suhas Vemuri <31122182+VSuhas22@users.noreply.github.com>
2018-03-03 21:09:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,14 @@
 console.log('Loaded!');
+var x=0;
 var element = document.getElementById('img');
 img.onclick=function(){
+    if(x===0)
+    {
     img.style.marginLeft='100px';
+    x=1;
+    }
+    else if(x==1)
+    {
+        img.style.marginLeft='0px';
+    }
 }
